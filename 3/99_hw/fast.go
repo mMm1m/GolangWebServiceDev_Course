7fffe93bfd2755471b7ddec1e8d47197a17183a8
Maxim Golchanskiy <golchanskiy.m.d@gmail.com>
2024-06-04 04:06:51 +0300

    Do function's work faster with profiler and easyjson library

--- 3/99_hw/fast.go
+++ 3/99_hw/fast.go
@@ -1,18 +1,46 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"hw3/easyjson"
 	"io"
+	"os"
+	"strings"
 )
 
 // вам надо написать более быструю оптимальную этой функции
 func FastSearch(out io.Writer) {
-	/*
-		!!! !!! !!!
-		обратите внимание - в задании обязательно нужен отчет
-		делать его лучше в самом начале, когда вы видите уже узкие места, но еще не оптимизировалм их
-		так же обратите внимание на команду в параметром -http
-		перечитайте еще раз задание
-		!!! !!! !!!
-	*/
-	SlowSearch(out)
+	user := easyjson.User{}
+	map_ := map[string]bool{}
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	fileContents := bufio.NewScanner(file)
+	fmt.Fprint(out, "found users:\n")
+	counter := 0
+	for fileContents.Scan() {
+		if err = user.UnmarshalJSON(fileContents.Bytes()); err != nil {
+			panic(err)
+		}
+		Android, MSIE := false, false
+		for _, browser := range user.Browsers {
+			if strings.Contains(browser, "Android") {
+				Android = true
+			} else if strings.Contains(browser, "MSIE") {
+				MSIE = true
+			} else {
+				continue
+			}
+			map_[browser] = true
+		}
+		if Android && MSIE {
+			email := strings.Replace(user.Email, "@", " [at] ", 1)
+			fmt.Fprintln(out, fmt.Sprintf("[%d] %s <%s>", counter, user.Name, email))
+		}
+		counter++
+	}
+	fmt.Fprintln(out, "\nTotal unique browsers", len(map_))
+
 }
